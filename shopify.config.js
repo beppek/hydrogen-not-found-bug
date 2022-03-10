498125afca7e5b06f82fa00dc7832569cde1eff2
Max Karlsson <beppe@live.se>
2022-03-10 17:53:44 +1100

    initial

---
+++ shopify.config.js
@@ -0,0 +1,6 @@
+export default {
+  storeDomain: import.meta.env?.PUBLIC_STORE_DOMAIN,
+  storefrontToken: import.meta.env?.PUBLIC_STOREFRONT_TOKEN,
+  storefrontApiVersion: '2022-04',
+  defaultLocale: 'en-au',
+};
